7b2bb54f592994e38c44a6bd7669351195853dcf
WhitedogWarren <werquin.lucas@wanadoo.fr>
2023-05-01 11:45:00 +0200

    style: :lipstick: a few style improvments

--- src/components/template/header/Header.tsx
+++ src/components/template/header/Header.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Avatar, IconButton, Menu, MenuItem } from "@mui/material";
 import { Link } from "react-router-dom";
+
+import { Avatar, IconButton, Menu, MenuItem } from "@mui/material";
 
 import { unsetLogs } from "../../../redux/authSlice";
 import { unsetServers } from "../../../redux/serversSlice";
@@ -27,34 +28,46 @@ export default function Header() {
 
     return (
         <div className="Header">
-            <h1>OpenWebChat</h1>
-            <div className="logpad">
-                <IconButton
-                    onClick={handleAvatarClick}
-                    aria-controls={ isAccountMenuOpen ? 'Votre compte' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={ isAccountMenuOpen ? "true" : undefined}
-                >
-                    {authStatus.user.picture && <Avatar alt="votre avatar" src={authStatus.user.picture} />}
-                    {!authStatus.user.picture && <Avatar>{authStatus.user.pseudo.substring(0, 1).toUpperCase()}</Avatar>}
-                </IconButton>
-                <Menu
-                    anchorEl={anchorEl}
-                    id="account-menu"
-                    open={isAccountMenuOpen}
-                    onClose={handleAvatarClose}
-                    onClick={handleAvatarClose}
-                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                >
-                    <MenuItem>
-                        <Link to="profile">Profil</Link>
-                    </MenuItem>
-                    <MenuItem onClick={() => logout()}>
-                        Déconnexion
-                    </MenuItem>
-                </Menu>
-            </div>
+            <h1>
+                <div className="logo-container">
+                    <img src="./images/openChatRooms.png" alt="logo openChatRooms" />
+                </div>
+                <div className="sitename">
+                    OpenWebChat
+                </div>
+                <div className="logpad-container">
+                    {authStatus.isLogged && (
+                        <div className="logpad">
+                            <IconButton
+                                onClick={handleAvatarClick}
+                                aria-controls={ isAccountMenuOpen ? 'Votre compte' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={ isAccountMenuOpen ? "true" : undefined}
+                            >
+                                {authStatus.user.picture && <Avatar alt="votre avatar" src={authStatus.user.picture} />}
+                                {!authStatus.user.picture && <Avatar>{authStatus.user.pseudo.substring(0, 1).toUpperCase()}</Avatar>}
+                                {authStatus.user.pseudo}
+                            </IconButton>
+                            <Menu
+                                anchorEl={anchorEl}
+                                id="account-menu"
+                                open={isAccountMenuOpen}
+                                onClose={handleAvatarClose}
+                                onClick={handleAvatarClose}
+                                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                            >
+                                <MenuItem>
+                                    <Link to="profile">Profil</Link>
+                                </MenuItem>
+                                <MenuItem onClick={() => logout()}>
+                                    <span className="logout-link" >Déconnexion</span>
+                                </MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                </div>
+            </h1>
         </div>
     )
 }
