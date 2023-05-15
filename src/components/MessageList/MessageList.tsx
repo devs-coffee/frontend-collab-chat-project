f418b2e45d01580e98b97e674b823f7cf960ff0b
julienb86 <julien.bouafia@orange.fr>
2023-05-15 20:49:50 +0200

    remove useless import

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,15 +1,26 @@
 import { IMessage } from '../../interfaces/IMessage';
 import './MessageList.scss';
 import Message from '../Message/Message';
+import { useEffect, useRef } from 'react';
 
 type messageList = {
-    messages: IMessage[]
+    messages: IMessage[],
 }
 
-export default function  MessageList( { messages } : messageList)  {
+const MessageList = ( { messages} : messageList) => {
+  const messageEndRef = useRef<null | HTMLDivElement>(null); 
+
+  useEffect(() => {
+      messageEndRef.current?.scrollIntoView({
+        behavior: "smooth"
+      })
+    },[messages]);
+
   return (
     <div className="MessageList">
         {messages && messages.map((message) => <Message message={message}/>)}
+        <div ref={messageEndRef} ></div>
     </div>
   );
 }
+export default MessageList;
