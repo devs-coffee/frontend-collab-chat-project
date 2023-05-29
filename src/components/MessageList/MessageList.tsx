6285759155ed12d00e8e61548e85ad1255807fe1
WhitedogWarren <werquin.lucas@wanadoo.fr>
2023-05-29 09:35:10 +0200

    feat: :sparkles: Get users avatars from messages list

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,6 +1,8 @@
+import { useSelector } from 'react-redux';
 import { Avatar } from '@mui/material';
 import { IMessage } from '../../interfaces/IMessage';
 import './MessageList.scss';
+import { User } from '../../interfaces/IUser';
 
 type messageList = {
     messages: IMessage[]
@@ -8,21 +10,32 @@ type messageList = {
 
 export default function  MessageList( { messages } : messageList)  {
     const currentDate = new Date(Date.now()).toLocaleString('fr', { dateStyle : 'long' });
-  return (
-    <div className="MessageList">
-        {messages && messages.map((message) => {
-            const messageDate = new Date(message.createdAt!).toLocaleString('fr', {dateStyle: 'long'});
-            const isToday = currentDate === messageDate;
-            return <div>
-                <div className='message' key={`message=${message.id}`}>
-                    {message.user?.picture && message.user.picture !== ''
-                    ? <Avatar alt="user picture" src={message.user.picture}></Avatar>  
-                    : <Avatar alt="user picture">{message.user?.pseudo.substring(0,1).toUpperCase()}</Avatar>}
-                    <p className="message_pseudo">{message.user?.pseudo} <span className='message_date'>{isToday ? "Aujourd'hui" : messageDate} à {new Date(message.createdAt!).getHours()}:{new Date(message.createdAt!).getMinutes()}</span></p>
+    const users = useSelector((state:any) => state.users);
+    const myUser = useSelector((state:any) => state.auth.user);
+    return (
+        <div className="MessageList">
+            {messages && messages.map((message) => {
+                const messageDate = new Date(message.createdAt!).toLocaleString('fr', {dateStyle: 'long'});
+                const isToday = currentDate === messageDate;
+                // console.log(message.user);
+                // console.log(users.data);
+                // console.log(users.data.find((user: User) => user.id === message.user?.id));
+                if(message.user!.id === myUser.id) {
+                    message.user!.picture = myUser.picture;
+                } else {
+                    message.user!.picture = users.data.find((user: User) => user.id === message.user?.id).picture;
+                }
+                
+                return <div key={`message=${message.id}`}>
+                    <div className='message' >
+                        {message.user?.picture && message.user.picture !== ''
+                        ? <Avatar alt="user picture" src={message.user.picture}></Avatar>  
+                        : <Avatar alt="user picture">{message.user?.pseudo.substring(0,1).toUpperCase()}</Avatar>}
+                        <p className="message_pseudo">{message.user?.pseudo} <span className='message_date'>{isToday ? "Aujourd'hui" : messageDate} à {new Date(message.createdAt!).getHours()}:{new Date(message.createdAt!).getMinutes()}</span></p>
+                    </div>
+                    <p key={message.id}>{message.content}</p>
                 </div>
-                <p key={message.id}>{message.content}</p>
-            </div>
-        })} 
-    </div>
-  );
+            })} 
+        </div>
+    );
 }
