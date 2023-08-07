74633824bcda5fb404aac4738dee4292fe82eaaa
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-08-07 14:23:49 +0200

    Configure & use the hook useDarkMode.ts
    - Maked DarkModeSwitch more short & clear
    
    - DarkModeSwitch accepts "darkMode" as a boolean prop and "setDarkMode" as a SetState function prop. (In our example, we pass the state and setState values obtained from our custom hook useDarkMode.)
    
    - This approach allows DarkModeSwitch to function as a PureComponent, avoiding re-running the hook every time an onChange event occurs."

--- src/components/template/header/Header.tsx
+++ src/components/template/header/Header.tsx
@@ -6,15 +6,17 @@ import { Avatar, IconButton, Menu, MenuItem } from "@mui/material";
 
 import { unsetLogs } from "../../../redux/authSlice";
 import { unsetServers } from "../../../redux/serversSlice";
-import DarkModeSwitch from "../../DarkModeSwitch/DarkModeSwitch";
+import { DarkModeSwitch} from "../../DarkModeSwitch/DarkModeSwitch";
+import { useDarkMode } from "../../../Hooks/useDarkMode";
 
 import './Header.scss';
 
-export default function Header({ioClose}:any) {
-    
+export default function Header({ ioClose }: any) {
+
     const dispatch = useDispatch();
-    const authStatus = useSelector((state:any) => state.authStatus);
-    const [anchorEl, setAnchorEl]  = useState<null | HTMLElement>(null);
+    const authStatus = useSelector((state: any) => state.authStatus);
+    const [darkMode, setDarkMode] = useDarkMode();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const isAccountMenuOpen = Boolean(anchorEl);
     const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
@@ -38,14 +40,13 @@ export default function Header({ioClose}:any) {
                     <Link to="/">OpenChatRooms</Link>
                 </div>
                 <div className="logpad-container">
-                    <DarkModeSwitch />
                     {authStatus.isLogged && (
                         <div className="logpad">
                             <IconButton
                                 onClick={handleAvatarClick}
-                                aria-controls={ isAccountMenuOpen ? 'Votre compte' : undefined}
+                                aria-controls={isAccountMenuOpen ? 'Votre compte' : undefined}
                                 aria-haspopup="true"
-                                aria-expanded={ isAccountMenuOpen ? "true" : undefined}
+                                aria-expanded={isAccountMenuOpen ? "true" : undefined}
                             >
                                 {authStatus.user.picture && <Avatar alt="votre avatar" src={authStatus.user.picture} />}
                                 {!authStatus.user.picture && <Avatar>{authStatus.user.pseudo.substring(0, 1).toUpperCase()}</Avatar>}
@@ -62,6 +63,12 @@ export default function Header({ioClose}:any) {
                             >
                                 <MenuItem>
                                     <Link to="profile">Profil</Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <DarkModeSwitch
+                                        darkMode={darkMode}
+                                        setDarkMode={setDarkMode}
+                                    />
                                 </MenuItem>
                                 <MenuItem onClick={() => logout()}>
                                     <span className="logout-link" >Déconnexion</span>
