24b6e54aa2a5347df8b7ef80f8ce6873af44c105
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-08-12 15:43:54 +0200

    Update IUser.tsx

--- src/interfaces/IUser.tsx
+++ src/interfaces/IUser.tsx
@@ -1,10 +1,13 @@
 import { Theme } from "./ITheme";
 
-export interface User {
-    id: string;
-    pseudo: string;
-    picture?: string;
+export interface Prefs {
     prefs?: {
         colorScheme: Theme | "LIGHT" | "DARK";
     }
+}
+
+export interface User extends Prefs {
+    id: string;
+    pseudo: string;
+    picture?: string;
 }
