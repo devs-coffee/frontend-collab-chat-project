66092966a6a242b87ae9b54bf9da04a3deedf5d4
Werquin Lucas <80154686+WhitedogWarren@users.noreply.github.com>
2023-08-15 13:07:53 +0200

    Delete src/components/auth directory

--- src/components/auth/Login/login.test.tsx
+++
@@ -1,8 +0,0 @@
-import { Login } from './Login';
-import { render } from '@testing-library/react';
-
-describe('Login', () => {
-    test('Should render without crash', async () => {
-        render(<Login />)
-    })
-})
