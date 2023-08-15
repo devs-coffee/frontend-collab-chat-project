be372357bff7a465f3880a349978baad463c4c7b
julienb86 <julien.bouafia@orange.fr>
2023-08-15 12:56:35 +0200

    fix

--- hooks/useIoSocket.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import IoSocketContext from "../contexts/IoSocketContext";
-const useIoSocket = () => {
-    return useContext(IoSocketContext);
-};
-
-export default useIoSocket;
