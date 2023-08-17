3ba169324f8a1bd13955fa69cf8e0db66ab477a1
WhitedogWarren <werquin.lucas@wanadoo.fr>
2023-08-17 10:54:21 +0200

    revert: commit

--- src/components/ServerDisplay/ServerMembersBox/ServerMembersBox.tsx
+++ src/components/ServerDisplay/ServerMembersBox/ServerMembersBox.tsx
@@ -1,8 +1,8 @@
-import { Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Stack } from "@mui/material";
+import { Snackbar, Stack } from "@mui/material";
 
 import { User } from "../../../interfaces/IUser";
 
@@ -10,23 +10,112 @@ import IoSocketContext from "../../../contexts/IoSocketContext";
 import { UserItem } from "../../../components";
 
 import "./ServerMembersBox.scss";
+import { ServerService } from "../../../services/serverService";
+import { AxiosError } from "axios";
+import { reduxData } from "../../../interfaces/IReduxData";
+import { Server } from "../../../interfaces/IServer";
+import { addOrUpdateServer } from "../../../redux/serversSlice";
+import { addUsers } from "../../../redux/usersSlice";
+
+const getServerUsers = async (serverId: string) => {
+    try {
+        const response = await new ServerService().getServerUsers(serverId);
+        return response.result;
+    } catch (error) {
+        let errorMessage = "membres du serveur non récupérés, veuillez réessayer";
+        if (error instanceof AxiosError) {
+            errorMessage = error.response?.data.message;
+        }
+        throw new Error(errorMessage);
+    }
+}
 
 interface ServerMemberBoxProps {
-	serverUsers: User[];
-	compareID: string; 
-	joinServer: () => void;
-	isDisabled: boolean;
-	setServerUsers: Dispatch<SetStateAction<User[]>>
+	//serverUsers: User[];
+	userId: string; 
+	//joinServer: () => void;
+	//isDisabled: boolean;
+	//setServerUsers: Dispatch<SetStateAction<User[]>>
 }
 
 export function ServerMembersBox(props: ServerMemberBoxProps): JSX.Element {
+	const dispatch = useDispatch();
+	const usersState = useSelector((state: any) => state.users);
 	const urlSearchParams = useParams();
 	const Socket = useContext(IoSocketContext)!.Socket;
-	const authStatus = useSelector((state:any) => state.authStatus);
-	const [connectedUsers, setConnectedUsers] = useState<string[]>([authStatus.user.id]);
+	const server = useSelector((state: reduxData) => state.servers.data.find((server: Server) => server.id === urlSearchParams.serverId));
+	const [serverUsers, setServerUsers] = useState<User[]>([]);
+	const [connectedUsers, setConnectedUsers] = useState<string[]>([props.userId]);
 	const [hasConnecetdUsers, setHasConnectedUsers] = useState<boolean>(false);
+	const [isDisabled, setIsDisabled] = useState<boolean>(false);
+    const [joinServerError, setJoinServerError] = useState<{ isError: boolean, errorMessage: string }>({ isError: false, errorMessage: '' });
+	const [hasFetchUsers, setHasFetchUsers] = useState<boolean>(false);
+	const [usersError, setUsersError] = useState<{ isError: boolean, errorMessage: string }>({ isError: false, errorMessage: '' });
+
+	const joinServer = async () => {
+        try {
+            setIsDisabled(true);
+            const response = await new ServerService().joinServer(urlSearchParams.serverId!);
+            const newServer = { ...server };
+            newServer.isCurrentUserMember = response.result
+            dispatch(addOrUpdateServer(newServer));
+            const usersData = await getServerUsers(urlSearchParams.serverId!)
+            let usersToAdd: User[] = [];
+            usersData.forEach((elt: User) => {
+                if (!usersState.data.find((user: User) => user.id === elt.id) && elt.id !== props.userId) {
+                    usersToAdd.push(elt);
+                }
+            })
+            dispatch(addUsers(usersToAdd));
+            setServerUsers(usersData);
+			setHasFetchUsers(true);
+            setIsDisabled(false)
+        } catch (error) {
+            let errorMessage: string;
+            if (error instanceof AxiosError) {
+                errorMessage = error.response?.data.message;
+            } else {
+                errorMessage = 'une erreur est survenue, veuillez réessayer';
+            }
+            setJoinServerError({ isError: true, errorMessage });
+            return;
+        }
+    }
+
+	const handleJoinToastClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setJoinServerError({ isError: false, errorMessage: '' });
+    }
+
+	const handleUsersToastClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setUsersError({ isError: false, errorMessage: '' });
+    }
 
 	useEffect(() => {
+		if(serverUsers.length === 0) {
+            getServerUsers(urlSearchParams.serverId!)
+                .then(response => {
+                    let usersToAdd: User[] = [];
+                    response.forEach((elt: User) => {
+                        if (!usersState.data.find((user: User) => user.id === elt.id) && elt.id !== props.userId) {
+                            usersToAdd.push(elt);
+                        }
+                    });
+                    dispatch(addUsers(usersToAdd));
+                    //setHasFetchUsers(true);
+                    setServerUsers(response);
+
+                })
+                .catch(error => {
+                    setServerUsers(error);
+                });
+        }
+		
 		if(!hasConnecetdUsers) {
 			Socket.emit('getServerConnectedUsers', {serverId: urlSearchParams.serverId!});
 		}
@@ -48,22 +137,17 @@ export function ServerMembersBox(props: ServerMemberBoxProps): JSX.Element {
         })
 
 		Socket.on('newMember', (data: {user:User}) => {
-			console.log('New member : ', data);
-			console.log(props.serverUsers);
-			console.log([data.user, ...props.serverUsers]);
-			let oldList = JSON.parse(JSON.stringify(props.serverUsers));
+			let oldList = JSON.parse(JSON.stringify(serverUsers));
 			oldList.push(data.user);
-			props.setServerUsers([data.user, ...oldList]);
-			//setConnectedUsers([data.userId, ...connectedUsers]);
-			//props.setServerUsers([])
+			setServerUsers([data.user, ...oldList]);
 		})
 
 		Socket.on('goneMember', (data: {user:User}) => {
 			console.log('Gone member : ', data);
-			console.log(props.serverUsers); // []
-			console.log(props.serverUsers.filter(elt => elt.id !== data.user.id));
-			let oldList = JSON.parse(JSON.stringify(props.serverUsers));
-			props.setServerUsers(oldList.filter((elt: User) => elt.id !== data.user.id));
+			console.log(serverUsers); // []
+			console.log(serverUsers.filter(elt => elt.id !== data.user.id));
+			let oldList = JSON.parse(JSON.stringify(serverUsers));
+			setServerUsers(oldList.filter((elt: User) => elt.id !== data.user.id));
 		})
 
         return () => {
@@ -77,21 +161,33 @@ export function ServerMembersBox(props: ServerMemberBoxProps): JSX.Element {
       }, [urlSearchParams, connectedUsers]
 	);
 
-	console.log('render');
+	console.log('MemberBox renders');
 	return (
 		<div className="members">
 			<h4 className="heading">Users :</h4>
 			<Stack className="stack" spacing={0.8}>
-				{props.serverUsers.map(user => (
+				{serverUsers.map(user => (
 					<UserItem key={`userBadge_${user.id}`} user={user} isConnected={connectedUsers.includes(user.id)}/>
 				))}
 			</Stack>
 
-			{props.serverUsers.length > 0 && (
-				<button className="joinOrLeaveButton" onClick={props.joinServer} disabled={props.isDisabled}>
-					{props.serverUsers.some(user => user.id === props.compareID) ? 'leave' : 'join'}
+			{serverUsers.length > 0 && (
+				<button className="joinOrLeaveButton" onClick={joinServer} disabled={isDisabled}>
+					{serverUsers.some(user => user.id === props.userId ) ? 'leave' : 'join'}
 				</button>
 			)}
+			<Snackbar
+				open={usersError.isError}
+				autoHideDuration={4000}
+				onClose={handleUsersToastClose}
+                message={/*getFullError()*/usersError.errorMessage}
+			/>
+			<Snackbar
+				open={joinServerError.isError}
+				autoHideDuration={4000}
+				onClose={handleJoinToastClose}
+				message={joinServerError.errorMessage}
+			/>
 		</div>
 	);
 }
--- src/pages/ServerDisplay/ServerDisplay.tsx
+++ src/pages/ServerDisplay/ServerDisplay.tsx
@@ -1,5 +1,5 @@
 import { AxiosError } from "axios";
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -8,9 +8,9 @@ import { Snackbar } from "@mui/material";
 import { ServerHeading, ServerChat, ServerChannelBox, ServerMembersBox, ServerUpdateForm } from "../../components";
 import { reduxData } from "../../interfaces/IReduxData";
 import { Server } from "../../interfaces/IServer";
-import { User } from "../../interfaces/IUser";
+//import { User } from "../../interfaces/IUser";
 import { addOrUpdateServer } from "../../redux/serversSlice";
-import { addUsers } from "../../redux/usersSlice";
+//import { addUsers } from "../../redux/usersSlice";
 import { ServerService } from "../../services/serverService";
 
 import './ServerDisplay.scss';
@@ -29,119 +29,130 @@ const getServerData = async (serverId: string) => {
     }
 }
 
-const getServerUsers = async (serverId: string) => {
-    try {
-        const response = await new ServerService().getServerUsers(serverId);
-        return response.result;
-    } catch (error) {
-        let errorMessage = "membres du serveur non récupérés, veuillez réessayer";
-        if (error instanceof AxiosError) {
-            errorMessage = error.response?.data.message;
-        }
-        throw new Error(errorMessage);
-    }
-}
+// const getServerUsers = async (serverId: string) => {
+//     try {
+//         const response = await new ServerService().getServerUsers(serverId);
+//         return response.result;
+//     } catch (error) {
+//         let errorMessage = "membres du serveur non récupérés, veuillez réessayer";
+//         if (error instanceof AxiosError) {
+//             errorMessage = error.response?.data.message;
+//         }
+//         throw new Error(errorMessage);
+//     }
+// }
 
 export function ServerDisplay() {
     const dispatch = useDispatch();
     const authStatus = useSelector((state: any) => state.authStatus);
-    const usersState = useSelector((state: any) => state.users);
+    //const usersState = useSelector((state: any) => state.users);
     const urlSearchParams = useParams();
     const server = useSelector((state: reduxData) => state.servers.data.find((server: Server) => server.id === urlSearchParams.serverId));
-    const [serverUsers, setServerUsers] = useState<User[]>([]);
+    //const [serverUsers, setServerUsers] = useState<User[]>([]);
+    //const [hasFetchUsers, setHasFetchUsers] = useState<boolean>(false);
     const [isUpdatingServer, setIsUpdatingServer] = useState<boolean>(false);
-    const [isDisabled, setIsDisabled] = useState<boolean>(false);
+    //const [isDisabled, setIsDisabled] = useState<boolean>(false);
     const [serverError, setServerError] = useState<string>('');
-    const [usersError, setUsersError] = useState<string>('');
-    const [joinServerError, setJoinServerError] = useState<{ isError: boolean, errorMessage: string }>({ isError: false, errorMessage: '' });
+    //const [usersError, setUsersError] = useState<string>('');
+    //const [joinServerError, setJoinServerError] = useState<{ isError: boolean, errorMessage: string }>({ isError: false, errorMessage: '' });
     const [mainContent, setMainContent] = useState<string>('chat');
     const [channelId, setChannelId] = useState<string>("");
 
-    const joinServer = async () => {
-        try {
-            setIsDisabled(true);
-            const response = await new ServerService().joinServer(urlSearchParams.serverId!);
-            const newServer = { ...server };
-            newServer.isCurrentUserMember = response.result
-            dispatch(addOrUpdateServer(newServer));
-            const serverData = await getServerUsers(urlSearchParams.serverId!)
-            let usersToAdd: User[] = [];
-            serverData.forEach((elt: User) => {
-                if (!usersState.data.find((user: User) => user.id === elt.id) && elt.id !== authStatus.user.id) {
-                    usersToAdd.push(elt);
-                }
-            })
-            dispatch(addUsers(usersToAdd));
-            setServerUsers(serverData);
-            setIsDisabled(false)
-        } catch (error) {
-            let errorMessage: string;
-            if (error instanceof AxiosError) {
-                errorMessage = error.response?.data.message;
-            } else {
-                errorMessage = 'une erreur est survenue, veuillez réessayer';
-            }
-            setJoinServerError({ isError: true, errorMessage });
-            return;
-        }
-    }
+    // const joinServer = async () => {
+    //     try {
+    //         setIsDisabled(true);
+    //         const response = await new ServerService().joinServer(urlSearchParams.serverId!);
+    //         const newServer = { ...server };
+    //         newServer.isCurrentUserMember = response.result
+    //         dispatch(addOrUpdateServer(newServer));
+    //         const serverData = await getServerUsers(urlSearchParams.serverId!)
+    //         let usersToAdd: User[] = [];
+    //         serverData.forEach((elt: User) => {
+    //             if (!usersState.data.find((user: User) => user.id === elt.id) && elt.id !== authStatus.user.id) {
+    //                 usersToAdd.push(elt);
+    //             }
+    //         })
+    //         dispatch(addUsers(usersToAdd));
+    //         setServerUsers(serverData);
+    //         setIsDisabled(false)
+    //     } catch (error) {
+    //         let errorMessage: string;
+    //         if (error instanceof AxiosError) {
+    //             errorMessage = error.response?.data.message;
+    //         } else {
+    //             errorMessage = 'une erreur est survenue, veuillez réessayer';
+    //         }
+    //         setJoinServerError({ isError: true, errorMessage });
+    //         return;
+    //     }
+    // }
 
     const handleDataToastClose = (event: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
         setServerError('');
-        setUsersError('');
+        //setUsersError('');
     }
 
-    const handleJoinToastClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setJoinServerError({ isError: false, errorMessage: '' });
-    }
+    // const handleJoinToastClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    //     if (reason === 'clickaway') {
+    //         return;
+    //     }
+    //     setJoinServerError({ isError: false, errorMessage: '' });
+    // }
 
-    const getFullError = (): ReactNode => {
-        return (
-            <>
-                {!server && (serverError)}
-                {!server && serverUsers.length < 1 && (<br />)}
-                {serverUsers.length < 1 && (usersError)}
-            </>
-        )
-    }
+    // const getFullError = (): ReactNode => {
+    //     return (
+    //         <>
+    //             {!server && (serverError)}
+    //             {!server && serverUsers.length < 1 && (<br />)}
+    //             {serverUsers.length < 1 && (usersError)}
+    //         </>
+    //     )
+    // }
 
     const avoidManagingChannel = () => {
         setMainContent('chat');
     }
 
     useEffect(() => {
-        getServerData(urlSearchParams.serverId!)
+        if(channelId === "") {
+            getServerData(urlSearchParams.serverId!)
             .then(response => {
+                console.log('got server data');
                 dispatch(addOrUpdateServer(response));
                 const channels = response.channels;
                 const defaultChannel = channels[0];
                 setChannelId(defaultChannel.id);
             })
             .catch(error => {
-                setUsersError(error);
-            });
-        getServerUsers(urlSearchParams.serverId!)
-            .then(response => {
-                let usersToAdd: User[] = [];
-                response.forEach((elt: User) => {
-                    if (!usersState.data.find((user: User) => user.id === elt.id) && elt.id !== authStatus.user.id) {
-                        usersToAdd.push(elt);
-                    }
-                });
-                dispatch(addUsers(usersToAdd));
-                setServerUsers(response);
-            })
-            .catch(error => {
                 setServerError(error);
             });
-    }, [urlSearchParams, authStatus, dispatch, usersState.data]);
+        }
+        
+        // if(!hasFetchUsers) {
+        //     getServerUsers(urlSearchParams.serverId!)
+        //         .then(response => {
+        //             let usersToAdd: User[] = [];
+        //             response.forEach((elt: User) => {
+        //                 if (!usersState.data.find((user: User) => user.id === elt.id) && elt.id !== authStatus.user.id) {
+        //                     usersToAdd.push(elt);
+        //                 }
+        //             });
+        //             dispatch(addUsers(usersToAdd));
+        //             setHasFetchUsers(true);
+        //             setServerUsers(response);
 
+        //         })
+        //         .catch(error => {
+        //             setServerError(error);
+        //         });
+        // }
+
+           
+    }, [urlSearchParams, authStatus, dispatch]);
+    console.log('ServerDisplay renders');
     return (
         <div className="ServerDisplay">
             {server === undefined ? <p>Patientez</p>
@@ -168,28 +179,28 @@ export function ServerDisplay() {
                             avoidManagingChannel={avoidManagingChannel}
                         />
                         <ServerMembersBox
-                            serverUsers={serverUsers}
-                            compareID={authStatus.user.id}
-                            joinServer={joinServer}
-                            isDisabled={isDisabled}
-                            setServerUsers={setServerUsers}
+                            //serverUsers={serverUsers}
+                            userId={authStatus.user.id}
+                            //joinServer={joinServer}
+                            //isDisabled={isDisabled}
+                            //setServerUsers={setServerUsers}
                         />
                     </div>
                 </>
             }
             {server && isUpdatingServer && (<ServerUpdateForm setIsUpdatingServer={setIsUpdatingServer} server={server} />)}
             <Snackbar
-                open={serverError !== '' || usersError !== ''}
+                open={serverError !== ''/* || usersError !== ''*/}
                 autoHideDuration={4000}
                 onClose={handleDataToastClose}
-                message={getFullError()}
+                message={/*getFullError()*/serverError}
             />
-            <Snackbar
+            {/* <Snackbar
                 open={joinServerError.isError}
                 autoHideDuration={4000}
                 onClose={handleJoinToastClose}
                 message={joinServerError.errorMessage}
-            />
+            /> */}
         </div>
     )
 }
